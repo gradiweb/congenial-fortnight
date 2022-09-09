f0d5e7e9628322618c1764dbeda38954ca003371
Miguel Soler <miguel@gradiweb.com>
2022-09-08 19:58:48 -0500

    Finis partial

--- parcial_miguel_soler.R
+++ parcial_miguel_soler.R
@@ -14,9 +14,28 @@ as.factor(`Day  Purchase`)
 
 
 ##4
+install.packages("arsenal")
+library(arsenal)
+df1 = data.frame(flowers)
+cost_flowers = df1 %>%
+  select(c('Cost.flowers'))
+  
+df2 = data.frame(flowers)
+day_purchase = df2 %>%
+  select(c('Day..Purchase'))
+
+
+comparedf(df1, df2)
+
+
 
 
 ##5
+data_flowers = data.frame(flowers)
+flowers_filter = filter(data_flowers, survived == 0)
+cv <- sd(data_flowers) / mean(data_flowers) * 100
+
+
 
 
 ##6
