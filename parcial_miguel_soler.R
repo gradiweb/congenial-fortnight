b7f65b2d8994d92186f0af2c8d0077c7ac739e11
Miguel Soler <miguel@gradiweb.com>
2022-09-08 19:48:17 -0500

    Calculate Mean

--- parcial_miguel_soler.R
+++ parcial_miguel_soler.R
@@ -1,5 +1,32 @@
+## 1
 install.packages("readxl")
 library("readxl")
-flowers <- read_excel("/Users/soler/Documents/personal/usta/exploratory/parcial/parcial/Clientes-Flores.xlsx")
-## View(flowers)
+flowers <- read_excel("/Users/soler/Documents/personal/usta/exploratory/parcial/parcial/Clientes-Flores_clean.xlsx")
+View(flowers)
 attach(flowers)
+
+##2
+as.factor(`Method  Payment`)
+as.factor(`Day  Purchase`)
+
+
+##3
+
+
+##4
+
+
+##5
+
+
+##6
+install.packages("dplyr")
+library(dplyr)
+data_table = tail(data.frame(flowers),20)
+customer_sex_filter = data_table %>%
+  select(c('Customer.Age'))
+colMeans(customer_sex_filter)
+
+
+
+
